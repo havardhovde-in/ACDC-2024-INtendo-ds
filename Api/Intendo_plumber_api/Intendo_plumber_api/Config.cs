417b27a34d2a823bc0ee707beb9902842afb7765
Mats Løvstrand Berntsen <mats.lovstrand.berntsen@innovasjonnorge.no>
2024-02-02 11:53:37 +0100

    Adding yml

---
+++ Api/Intendo_plumber_api/Intendo_plumber_api/Config.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Intendo_plumber_api;
+public class Config
+{
+  public string PredictionKey { get; set; }
+  public Guid ProjectId { get; set; }
+  public string IterationName { get; set; }
+  public string PredictionEndpoint { get; set; }
+}
---
+++ Api/Intendo_plumber_api/Intendo_plumber_api/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Intendo_plumber_api.Startup))]
+
+namespace Intendo_plumber_api
+{
+
+  public class Startup : FunctionsStartup
+  {
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+      var context = builder.GetContext();
+
+      builder.Services.AddOptions<Config>()
+              .Configure<IConfiguration>((settings, configuration) =>
+              {
+                configuration.GetSection("Config").Bind(settings);
+              });
+    }
+  }
+}
