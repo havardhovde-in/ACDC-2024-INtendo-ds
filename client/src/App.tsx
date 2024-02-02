f9daddce6a90c9eb4a8bceadd6d6391577931b94
Ingvild Unander Netland <ingvild.unander.netland@innovasjonnorge.no>
2024-02-02 20:33:00 +0100

    Update favicon and add background images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import Background from "./assets/background-clouds.png";
+import BackgroundLeft from "./assets/lg-textured.png";
+import BackgroundRight from "./assets/sm-plain.png";
 
 function App() {
   return (
     <div className="App">
+      <img className="App__background"src={Background}></img>
+      <img className="App__bgimage_left"src={BackgroundLeft}></img>
+      <img className="App__bgimage_right"src={BackgroundRight}></img>
+
+     
       <Home />
     </div>
   );
