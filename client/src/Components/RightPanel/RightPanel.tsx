83b38c0f6e772eb532b3d7e7cc06c213885e59b4
Mats Løvstrand Berntsen <mats.lovstrand.berntsen@innovasjonnorge.no>
2024-02-03 13:39:38 +0100

    plz håvard?

--- client/src/Components/RightPanel/RightPanel.tsx
+++ client/src/Components/RightPanel/RightPanel.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import "./RightPanel.scss";
 import { Order } from "../../Types/Orders";
 import OrderList from "../OrderList/OrderList";
+import { Button } from "@fluentui/react-components";
 
 type RightPanelTypes = {
   order: Order;
 };
 
+
+///Håvard, kan du legge til funksjonalitet her som endrer order state til accepted?
 const RightPanel: React.FC<RightPanelTypes> = ({ order }) => {
   return (
     <div className="right-panel  white-background">
       <div className="right-panel__menu">
         <OrderList order={order} />
       </div>
-    </div>
+      <Button appearance="primary" >
+          Analyze
+        </Button>
+            </div>
   );
 };
 
